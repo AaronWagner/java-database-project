38d68c6c164536ab00ff5c840d93d46e57fedd0d
AaronWagner <aaron.p.wagner@gmail.com>
2014-11-26 19:40:29 -0800

    Beginning views and other utility methods

--- sel_prdct.java
+++ sel_prdct.java
@@ -3,11 +3,20 @@ import java.sql.*;
 
 
 //hello world
-class sel_prdct
+class DataProject
 {
+   ArrayList<String> courseNumbers;
+
+
+
+
+
+
    public static void main (String args[])  //throws SQLException
    
    {
+      DataProject myDataProject=newDataProject();
+      
       try
       {
          DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
@@ -15,29 +24,50 @@ class sel_prdct
       catch (Exception e)
       {
          System.out.print(e.getStackTrace() +"\n");
-         
+       
       }
+      initalizeValues() 
+       
+   }      
+   
+   void initalizeValues()
+   {
+      //initalizes courses ALL user inputed courses should be checked as valid via the testCourseInputMethod
+      ResultSet courseResult=makeQuery("select course_number from course");
+      while (courseResult.next())
+      
+   }
+   
+      
+   ResultSet makeQuery(String query)
+   {
+      ResultSet output;
+   
       try
       {
          Connection myconnection = DriverManager.getConnection("jdbc:oracle:thin:@olympia.unfcsd.unf.edu:1521:dworcl", "teama5dm2f14", "team5ghjptw");
          
          Statement mystatment=myconnection.createStatement();
          ResultSet rset = mystatment.executeQuery ("select * from Course");   //read javadocs for ResultsSet
-         
-         while (rset.next())
-         {
-            System.out.println(rset.getString(1)+" "+rset.getString(2)+". . . \n");
-         }
+                     
       }
       catch (Exception e)
       {
          System.out.println(e.getMessage());
          System.out.println(e.getStackTrace().toString());
-      }   
-   
+      }
+      
    }
 
-
-
-
+   void displayCourseRequest (String course)
+   {
+      String studentQuery=("Select * from student_request WHERE course_number = '"+course+"'"; 
+      ResultSet studentResults=makeQuery(studentQuery);
+      //dispalyResults(studentResults);
+      String facultyQuery=("Select * from faculty_request WHERE course_number = '"+course+"'"; 
+      ResultSet facultyResults=makeQuery(studentQuery);
+      //dispalyResults(studentResults);
+   }
+   
+   
 }
